fc29d204f98190e565718d75bbc7b65a8b092d72
Ian Foster <ian.foster@me.com>
2020-01-12 12:07:16 -0500

    Added questions to STORE in js

---
+++ script.js
@@ -0,0 +1,90 @@
+const STORE = [
+    //1
+    {
+        question: 'What year did Batman first appear in comics?',
+        answers: [
+            '1939',
+            '1945',
+            '1922',
+            '1968'
+        ],
+        correctAnswer:'1939'
+    },
+    //2
+    {
+        question: 'What is the name of Batman\'s car?',
+        answers: [
+            'The Batcar',
+            'The Batwhip',
+            'The Batmobile',
+            'The Batwheel'
+        ],
+        correctAnswer: 'The Batmobile'
+    },
+    //3
+    {
+        question: 'Who is Batman\'s sidekick?',
+        answers: [
+            'Batboy',
+            'Sparrow',
+            'Speedy',
+            'Robin'
+        ],
+        correctAnswer: 'Robin'
+    },
+    //4
+    {
+        question: 'Besides fighting crime, what does Batman do for his day job?',
+        answers: [
+            'Reporter at the Daily Planet',
+            'CEO of Wayne Enterprises',
+            'Owner of Queen Industries',
+            'Software developer at S.T.A.R. Labs'
+        ],
+        correctAnswer: 'CEO of Wayne Enterprises'
+    },
+    //5
+    {
+        question: 'Who of the following have been Batman?',
+        answers: [
+            'James Gordon',
+            'Bruce Wayne',
+            'Dick Grayson',
+            'All of the above'
+        ],
+        correctAnswer: 'All of the above'
+    },
+    //6
+    {
+        question: 'In what year did Batman make his first movie appearance?',
+        answers: [
+            '1989',
+            '1966',
+            '2005',
+            '1945'
+        ],
+        correctAnswer: '1966'
+    },
+    //7
+    {
+        question: 'What is the name of Bruce Wayne\'s loyal butler?',
+        answers: [
+            'Manfred Buttersworth',
+            'Alfred Pennyworth',
+            'Richard Dollarsby',
+            'Alfred Pennywise'
+        ],
+        correctAnswer: 'Alfred Pennyworth'
+    },
+    //8
+    {
+        question: 'Which dynamic duo created Batman?',
+        answers: [
+            'Stan Lee & Jack Kirby',
+            'Jerry Siegel & Joe Shuster',
+            'Bob Kane & Bill Finger',
+            'Paul Norris & Mort Weisinger'
+        ],
+        correctAnswer: 'Bob Kane & Bill Finger'
+    }
+]
